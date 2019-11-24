6d3e2d59b6366cfeb1d07502cbbedc8236743d9b
Bogdan 'TiBx' Savenko <savenko.bogdan@gmail.com>
2019-11-25 01:52:47 +0200

    added partial template specialization for std::string, std::vector and std::list

--- print_ip.cpp
+++ print_ip.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <list>
 
 const int c_byteSize = 8;
 
@@ -22,11 +22,48 @@ void print_ip(T param)
 	std::cout << std::endl;
 }
 
+template<>
+void print_ip(std::string param)
+{
+	std::cout << param << std::endl;
+}
+
+template<typename T>
+void print_ip(std::vector<T> param)
+{
+	for (auto it = param.begin(); it != param.end(); ++it)
+	{
+		if (it != param.begin())
+		{
+			std::cout << '.';
+		}
+		std::cout << *it;
+	}
+	std::cout << std::endl;
+}
+
+template<typename T>
+void print_ip(std::list<T> param)
+{
+	for (auto it = param.begin(); it != param.end(); ++it)
+	{
+		if (it != param.begin())
+		{
+			std::cout << '.';
+		}
+		std::cout << *it;
+	}
+	std::cout << std::endl;
+}
+
 int main(/*int argc, char const *argv[]*/)
 {
     print_ip(char(-1));
 	print_ip(short(0));
 	print_ip(int(2130706433));
 	print_ip(int64_t(8875824491850138409));
+	print_ip(std::string("8.8.8.8"));
+	print_ip(std::vector<int>{ 192, 168, 0, 1 });
+	print_ip(std::list<int>{ 192, 168, 1, 1 });
     return 0;
 }
