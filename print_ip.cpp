8948179e1d6d3392aa2775e3dff7ea7a05ca56bc
Bogdan 'TiBx' Savenko <savenko.bogdan@gmail.com>
2019-11-26 21:55:48 +0200

    added std::ostream as parameter for print_ip function instead of using std::cout directly

--- print_ip.cpp
+++ print_ip.cpp
@@ -8,62 +8,62 @@
 const int c_byteSize = 8;
 
 template<typename T>
-void print_ip(T param)
+void print_ip(T param, std::ostream& os)
 {
 	for (int i = sizeof(T) - 1; i >= 0; --i)
 	{
 		if (i != sizeof(T) - 1)
 		{
-			std::cout << '.';
+			os << '.';
 		}
 		uint8_t value = param >> i * c_byteSize;
-		std::cout << int(value);
+		os << int(value);
 	}
-	std::cout << std::endl;
+	os << std::endl;
 }
 
 template<>
-void print_ip(std::string param)
+void print_ip(std::string param, std::ostream& os)
 {
-	std::cout << param << std::endl;
+	os << param << std::endl;
 }
 
 template<typename T>
-void print_ip(std::vector<T> param)
+void print_ip(std::vector<T> param, std::ostream& os)
 {
 	for (auto it = param.begin(); it != param.end(); ++it)
 	{
 		if (it != param.begin())
 		{
-			std::cout << '.';
+			os << '.';
 		}
-		std::cout << *it;
+		os << *it;
 	}
-	std::cout << std::endl;
+	os << std::endl;
 }
 
 template<typename T>
-void print_ip(std::list<T> param)
+void print_ip(std::list<T> param, std::ostream& os)
 {
 	for (auto it = param.begin(); it != param.end(); ++it)
 	{
 		if (it != param.begin())
 		{
-			std::cout << '.';
+			os << '.';
 		}
-		std::cout << *it;
+		os << *it;
 	}
-	std::cout << std::endl;
+	os << std::endl;
 }
 
 int main(/*int argc, char const *argv[]*/)
 {
-    print_ip(char(-1));
-	print_ip(short(0));
-	print_ip(int(2130706433));
-	print_ip(int64_t(8875824491850138409));
-	print_ip(std::string("8.8.8.8"));
-	print_ip(std::vector<int>{ 192, 168, 0, 1 });
-	print_ip(std::list<int>{ 192, 168, 1, 1 });
+    print_ip(char(-1), std::cout);
+	print_ip(short(0), std::cout);
+	print_ip(int(2130706433), std::cout);
+	print_ip(int64_t(8875824491850138409), std::cout);
+	print_ip(std::string("8.8.8.8"), std::cout);
+	print_ip(std::vector<int>{ 192, 168, 0, 1 }, std::cout);
+	print_ip(std::list<int>{ 192, 168, 1, 1 }, std::cout);
     return 0;
 }
